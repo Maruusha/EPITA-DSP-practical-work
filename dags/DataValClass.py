cf1e5d5e78d9776abd389812e54d05460b5cbac9
Maruusha <arcturus6998@gmail.com>
2026-03-25 11:30:37 +0100

    update validate_data()

--- dags/DataValClass.py
+++ dags/DataValClass.py
@@ -3,17 +3,18 @@ from typing import List, Dict
 
 class DataValClass(BaseModel):
     # actual data
-    records: List[Dict]
-    good_records: List[Dict]
-    bad_records: List[Dict]
+    records: List[Dict] = []
+    good_records: List[Dict] = []
+    bad_records: List[Dict] = []
     
     # File info
-    source_filename: str
+    source_filename: str = "no_file"
     is_processed: bool = False
-    error_criticality: str
+    error_criticality: str = "None"
+
     
     # Validation info
     is_schema_valid: bool = True
-    schema_missing_column: List[str]
+    schema_missing_column: List[str] = []
     total_rows: int = 0
     error_count: int = 0
--- dags/ingestion_validate_data.py
+++ dags/ingestion_validate_data.py
@@ -7,6 +7,7 @@ import pandas as pd
 import pendulum
 from airflow.sdk import dag, task
 import great_expectations as gx
+import great_expectations.expectations as gxe
 
 from DataValClass import DataValClass
 
@@ -15,6 +16,7 @@ VALID_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
 VALID_MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 VALID_SEASONS = ['Spring', 'Summer', 'Fall', 'Winter']
 VALID_SOURCES = ['Wind', 'Solar', 'Mixed'] 
+COLUMNS = ['Date', 'Start_Hour', 'End_Hour', 'Source', 'Day_of_Year', 'Day_Name', 'Month_Name', 'Season', 'Production']
 
 @dag(
     dag_id='ingestion_validate_data',
@@ -27,20 +29,20 @@ VALID_SOURCES = ['Wind', 'Solar', 'Mixed']
 def ingestion_validate_data():
     @task
     def read_data() -> dict:    
-        folderpath = '../data/raw_data/'
+        folderpath = '/opt/airflow/data/raw_data/'
         csv_files = [f for f in os.listdir(folderpath) if f.endswith('.csv')]
         if csv_files:
             filename = random.choice(csv_files)
             full_path = os.path.join(folderpath, filename)
 
-            input_data_df = pd.read_csv(full_path)
-            logging.info(f'Extract {input_data_df.size} rows from the file {filename}')
+            data_to_ingest_df = pd.read_csv(full_path)
+            df_row_len = data_to_ingest_df.shape[0]
+            logging.info(f'Extract {df_row_len} rows from the file {filename}')
 
             # Handle empty file
-            if input_data_df.size < 1:
+            if df_row_len < 1:
                 return {}
             
-            data_to_ingest_df = input_data_df.sample(n=input_data_df.size)
             data_obj = DataValClass(
                         records=data_to_ingest_df.to_dict(orient="records"),
                         source_filename=filename,
@@ -60,70 +62,57 @@ def ingestion_validate_data():
         payload = DataValClass(**data_to_ingest)
         df = pd.DataFrame(payload.records)
 
-        # Initialize GX Context
         context = gx.get_context()
-        datasource = context.sources.add_pandas(name="my_pandas_datasource")
+        suite_name = "dsp_validation_suite"
+
+        try:
+            suite = context.suites.get(name=suite_name)
+        except:
+            suite = context.suites.add(gx.ExpectationSuite(name=suite_name))
+
+        suite.add_expectation(gxe.ExpectTableColumnsToMatchSet(column_set=COLUMNS))
+        for col in COLUMNS:
+            suite.add_expectation(gxe.ExpectColumnValuesToNotBeNull(column=col))
+        for col in ['Start_Hour', 'End_Hour', 'Day_of_Year', 'Production']:
+            suite.add_expectation(gxe.ExpectColumnValuesToBeInTypeList(
+                column=col, type_list=['int', 'float', 'int64', 'float64']
+            ))
+        suite.add_expectation(gxe.ExpectColumnValuesToBeInSet(column='Day_Name', value_set=VALID_DAYS))
+        suite.add_expectation(gxe.ExpectColumnValuesToBeInSet(column='Month_Name', value_set=VALID_MONTHS))
+        suite.add_expectation(gxe.ExpectColumnValuesToBeInSet(column='Season', value_set=VALID_SEASONS))
+        suite.add_expectation(gxe.ExpectColumnValuesToBeInSet(column='Source', value_set=VALID_SOURCES))
+        suite.add_expectation(gxe.ExpectColumnValuesToBeBetween(column="Start_Hour", min_value=0, max_value=24))
+        suite.add_expectation(gxe.ExpectColumnValuesToBeBetween(column="End_Hour", min_value=0, max_value=24))
+        suite.add_expectation(gxe.ExpectColumnValuesToBeBetween(column="Day_of_Year", min_value=1, max_value=366))
+        suite.add_expectation(gxe.ExpectColumnValuesToBeBetween(column="Production", min_value=0, max_value=25000))
+        suite.add_expectation(gxe.ExpectColumnValuesToMatchStrftimeFormat(column="Date", strftime_format="%m/%d/%Y"))
+
+        datasource = context.data_sources.add_pandas(name="my_pandas_datasource")
         asset = datasource.add_dataframe_asset(name="my_df_asset")
+        batch_definition = asset.add_batch_definition_whole_dataframe("my_batch")
 
-        # Define Expectations Rules
-        batch_request = asset.build_batch_request(dataframe=df)
-        expectation_suite_name = "dsp_validation_suite"
-        context.add_or_update_expectation_suite(expectation_suite_name)
-        
-        validator = context.get_validator(
-            batch_request=batch_request,
-            expectation_suite_name=expectation_suite_name,
+        validation_definition = context.validation_definitions.add(
+            gx.ValidationDefinition(
+                name="my_validation",
+                data=batch_definition,
+                suite=suite,
+            )
         )
+        results = validation_definition.run(batch_parameters={"dataframe": df})
 
-        full_col_list = ['Date', 'Start_Hour', 'End_Hour', 'Source', 'Day_of_Year', 'Day_Name',	'Month_Name', 'Season', 'Production']
-
-        # Schema / Missing Columns
-        schema_res = validator.expect_table_columns_to_match_set(column_set=full_col_list)
-
-        # Completeness, Type
-        for col in full_col_list:
-            validator.expect_column_values_to_not_be_null(column=col) # Completeness
-            # Type (Columns should be numeric stay numeric), 'Validity' (There is no xyz day)
-            if col in ['Start_Hour', 'End_Hour', 'Day_of_Year', 'Production']:
-                validator.expect_column_values_to_be_in_type_list(
-                    column=col, type_list=['int', 'float', 'int64', 'float64']
-                )
-        validator.expect_column_values_to_be_in_set(column='Day_Name', value_set=VALID_DAYS)
-        validator.expect_column_values_to_be_in_set(column='Month_Name', value_set=VALID_MONTHS)
-        validator.expect_column_values_to_be_in_set(column='Season', value_set=VALID_SEASONS)
-        validator.expect_column_values_to_be_in_set(column='Source', value_set=VALID_SOURCES)
-
-        # Outlier, ImpossibleValue
-        validator.expect_column_values_to_be_between(column="Start_Hour", min_value=0, max_value=24) 
-        validator.expect_column_values_to_be_between(column="End_Hour", min_value=0, max_value=24) 
-        validator.expect_column_values_to_be_between(column="Day_of_Year", min_value=1, max_value=366) 
-        validator.expect_column_values_to_be_between(column="Production", min_value=0, max_value=25000) # Max value is 23k
-        validator.expect_column_values_to_match_strftime_format(column="Date", strftime_format="%m/%d/%Y")
-        # Using pendulum for timezone consistency with Airflow
-        now_str = pendulum.now("UTC").strftime('%m/%d/%Y')
-        validator.expect_column_values_to_be_between(
-            column="Date",
-            min_value="01/01/2018",       
-            max_value=now_str,            # Current date
-            parse_strings_as_datetime=True,
-            allow_cross_type_comparisons=True
-        )
-
-        # Criticality calculation
-        results = validator.validate()
-
-        # Use a SET to store unique row indices that failed
         bad_indices = set()
+        payload.is_schema_valid = True
         for r in results.results:
             if not r.success:
-                # GX returns 'unexpected_index_list' if the data is a DataFrame
                 indices = r.result.get('unexpected_index_list', [])
                 bad_indices.update(indices)
+            if r.expectation_config.type == "expect_table_columns_to_match_set":
+                payload.is_schema_valid = r.success
+
         payload.error_count = len(bad_indices)
         error_rate = payload.error_count / payload.total_rows
 
-        # Determine Criticality based on rules
-        if not schema_res.success or error_rate > 0.50:
+        if not payload.is_schema_valid or error_rate > 0.50:
             payload.error_criticality = "High"
         elif 0.10 <= error_rate <= 0.50:
             payload.error_criticality = "Medium"
@@ -132,16 +121,13 @@ def ingestion_validate_data():
         else:
             payload.error_criticality = "None"
 
-        payload.is_schema_valid = schema_res.success
         payload.is_processed = True
-
-        # Split the DataFrame using the indices
         bad_df = df.iloc[list(bad_indices)]
         good_df = df.drop(index=list(bad_indices))
         payload.bad_records = bad_df.to_dict(orient="records")
         payload.good_records = good_df.to_dict(orient="records")
-     
-        return payload.model_dump() 
+
+        return payload.model_dump()
 
     # TODO - Dev task 
     @task
@@ -164,8 +150,8 @@ def ingestion_validate_data():
             return 
         
         payload = DataValClass(**data_to_ingest)
-        good_folder = '../data/good_data/'
-        bad_folder = '../data/bad_data/'
+        good_folder = '/opt/airflow/data/good_data/'
+        bad_folder = '/opt/airflow/data/bad_data/'
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         base_name = payload.source_filename.replace('.csv', '')
 
