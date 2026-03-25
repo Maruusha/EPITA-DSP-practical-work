2dcafcd1e6250f9532af3605ca648bebdb6e7101
Maruusha <arcturus6998@gmail.com>
2026-03-25 12:38:21 +0100

    Feature/dag ingestion validate (#28)
    * create validate_data DAG
    
    * validate data dag
    
    * Update corruptDataset_schema.py
    
    * Untest - Validate data and DataValClass.py
    
    * update validate_data.py
    
    * Update docker file
    
    * Update docker-compose.yml
    
    * revert docker compose yml
    
    * update text
    
    * update Dockerfile to avoid modifying airflow working dir
    
    * fix link between airflow Dockerfile and docker-compose
    
    * update validate_data()
    
    * update split data() to create log file also
    
    * Delete split_data_1_2026-03-25_10-48-43.txt
    
    * Update .gitignore
    
    * update error rate
    
    ---------
    
    Co-authored-by: Tran Duy Thai <duy-thai.tran@epita.fr>

---
+++ dags/DataValClass.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import List, Dict
+
+class DataValClass(BaseModel):
+    # actual data
+    records: List[Dict] = []
+    good_records: List[Dict] = []
+    bad_records: List[Dict] = []
+    
+    # File info
+    source_filename: str = "no_file"
+    is_processed: bool = False
+    error_criticality: str = "None"
+
+    
+    # Validation info
+    is_schema_valid: bool = True
+    schema_missing_column: List[str] = []
+    schema_missing_column_count: int = 0
+    total_rows: int = 0
+    error_count: int = 0
+    error_rate: float = 0
